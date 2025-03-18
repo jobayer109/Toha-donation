8f1e274710e81fb60a6dc5d49602fbda15d8c280
jobayer <jobayer.ahmed109@gmail.com>
2025-03-18 18:56:16 +0600

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ import { useState, useEffect } from "react";
 
 
 export default function Home() {
-  const [currentUrl, setCurrentUrl] = useState("");
+  const [currentUrl, setCurrentUrl] = useState("https://toha-donation.vercel.app/");
 
   useEffect(() => {
     if (typeof window !== "undefined") {
